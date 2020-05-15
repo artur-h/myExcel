74734fee73af8f67396326dd68f1abeb371552cc
artur-h <artur.hlushko@gmail.com>
2020-05-15 12:29:24 +0300

    First cell will be selected when the table has been loaded

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,7 @@ import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.component';
 import {resizeHandler} from '@/components/table/table.resize';
 import {shouldResize} from '@/components/table/table.functions';
+import {TableSelection} from '@/components/table/TableSelection';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -15,6 +16,17 @@ export class Table extends ExcelComponent {
 
   toHTML() {
     return createTable(32);
+  }
+
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
   }
 
   onMousedown(event) {
