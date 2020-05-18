48a1e0398def36178d61d2f15c617425b42a7ac4
artur-h <artur.hlushko@gmail.com>
2020-05-18 21:06:37 +0300

    Can manage state of btn in toolbar

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -97,7 +97,6 @@ export class Table extends ExcelComponent {
   }
 
   onInput(event) {
-    // this.$emit('table:input', $(event.target));
     this.updateTextInStore($(event.target).text());
   }
 }
